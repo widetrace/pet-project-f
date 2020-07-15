3648868b7ba89e1c061822abe51da1ed6378d0aa
widetrace <widetrace@icloud.com>
2020-07-15 16:25:26 +0300

    Next game info display

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -5,16 +5,20 @@
     </h1>
     <div class="team-away">
       <img :src="awayTeamLogo" alt="Away team logo" />
-      <p>{{ game.teams.away.score }}</p>
+      <p v-if="gameStatus == 'previous'">{{ game.teams.away.score }}</p>
     </div>
     <div class="team-home">
       <img :src="homeTeamLogo" alt="Home team logo" />
-      <p>{{ game.teams.home.score }}</p>
+      <p v-if="gameStatus == 'previous'">{{ game.teams.home.score }}</p>
     </div>
+    <p class="date">{{ date }}</p>
+    <p class="time">{{ time }}</p>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   props: ["game", "gameStatus"],
   computed: {
@@ -23,20 +27,47 @@ export default {
     },
     homeTeamLogo() {
       return this.$store.getters["logo/url"](this.game.teams.home.team.id);
+    },
+    date() {
+      return moment(this.game.gameDate).format("DD.MM.YYYY");
+    },
+    time() {
+      return moment(this.game.gameDate).format("HH:mm")
     }
+  },
+  created() {
+    console.log(this.game);
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .game {
+  // height: 75vh;
+  border-radius: 4px;
+  box-shadow: 0 1px 0 0 #d7d8db, 0 0 0 1px #e3e4e8;
+  background-color: white;
   display: grid;
-  grid-template-areas: 
-  "title title"
-  "away home";
+  grid-template-rows: 
+    minmax(75px, 0.33fr)
+    minmax(150px, 1.67fr)
+    minmax(50px, 0.33fr);
+  grid-template-areas:
+    "title title"
+    "away home"
+    "date time";
   h1 {
+    margin: 0;
+    padding: 10px;
     grid-area: title;
   }
+}
+
+.team-away, .team-home {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .team-away {
@@ -45,6 +76,14 @@ export default {
 
 .team-home {
   grid-area: home;
+}
+
+.date {
+  grid-area: date;
+}
+
+.time {
+  grid-area: time;
 }
 
 img {
