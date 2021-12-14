72e4d2295204983c6365c90fd46d841a80f78452
widetrace <widetrace@icloud.com>
2021-12-14 09:44:58 +0300

    installed pug

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+//- new component
+#nav
+  router-link(:to="{ name: 'Home' }") |
+  router-link(:to="{ name: 'About' }")
+router-view
 </template>
 
 <style lang="scss">
