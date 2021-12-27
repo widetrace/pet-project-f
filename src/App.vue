a072fa44b100f0854cea54c0f51af127d0ebedad
widetrace <widetrace@icloud.com>
2021-12-27 14:45:13 +0300

    header carousel
    Get ready for setting up carousel in header

--- src/App.vue
+++ src/App.vue
@@ -41,16 +41,12 @@ export default {
     "nav main"
     "footer footer";
   grid-template-columns: 0.66fr 3fr;
-  grid-template-rows: 115px 4fr 100px;
+  // grid-template-rows: 115px 4fr 100px;
   grid-gap: 1rem;
 }
 
 #header {
   grid-area: header;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  align-items: center;
 }
 
 #content,
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,52 @@
 <template lang="pug">
 #header
-  img(:src="leagueIcon")
-  h1 Blackhawks web app
-  img(
-    src="https://www-league.nhlstatic.com/images/logos/teams-current-primary-dark/16.svg"
-  )
+  #header-title
+    img(:src="leagueIcon")
+    h1 Blackhawks web app
+    img(
+      src="https://www-league.nhlstatic.com/images/logos/teams-current-primary-dark/16.svg"
+    )
+  #header-schedule
+    //- Carousel(:items-to-show="2.5", v-if="data")
+    //-   Slide(v-for="(item, index) in data.dates", :key="index")
+    //-     MatchBlock(:item="item", :isHeader="true")
 </template>
 
 <script>
+import { computed, reactive, toRefs } from 'vue';
+import { useStore } from 'vuex';
+
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+import MatchBlock from '@/components/MatchBlock.vue';
+
 export default {
+  components: {
+    Carousel,
+    Slide,
+    MatchBlock,
+  },
   setup() {
     // eslint-disable-next-line global-require
     const leagueIcon = require('@/assets/LeagueIcon.svg');
 
+    const state = reactive({
+      data: null,
+    });
+
+    const store = useStore();
+
+    // get ready for carousel
+    state.data = computed(() => {
+      if (store.getters['schedule/games']) {
+        return store.getters['schedule/games'];
+      }
+      return null;
+    });
+
     return {
+      ...toRefs(state),
       leagueIcon,
     };
   },
@@ -25,7 +58,17 @@ export default {
   border-radius: 4px;
   box-shadow: 0 1px 0 0 #d7d8db, 0 0 0 1px #e3e4e8;
   background-color: white;
-  padding: 0 10%;
+  display: flex;
+  flex-flow: column nowrap;
+
+  &-title {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10%;
+  }
+
   img {
     height: 100px;
   }
